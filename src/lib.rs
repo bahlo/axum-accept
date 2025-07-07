e1e632cdfcaf538cb722802fa75cf566ffc96397
Arne Bahlo <hey@arne.me>
2025-07-07 14:51:42 +0200

    fix: Add ordering, test suite

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 //! # Example
 //!
 //! ```rust
+//! use axum::{extract::Json, response::{IntoResponse, Response}};
+//! use axum_accept::{typed_media_type, Accept2};
+//! use serde::Serialize;
+//!
 //! typed_media_type!(TextPlain: TEXT/PLAIN);
 //! typed_media_type!(ApplicationJson: APPLICATION/JSON);
 //!
@@ -12,7 +16,7 @@
 //!     content: String,
 //! }
 //!
-//! async fn my_handler(accept: Accept2<TextPlain, ApplicationJson) -> Response {
+//! async fn my_handler(accept: Accept2<TextPlain, ApplicationJson>) -> Response {
 //!     match accept {
 //!         Accept2::A(TextPlain(_)) => "hello world".into_response(),
 //!         Accept2::B(ApplicationJson(_)) => Json(Message { content: "hello_world".to_string() }).into_response(),
@@ -23,12 +27,14 @@
 #![deny(clippy::pedantic, clippy::unwrap_used)]
 #![deny(missing_docs)]
 
+use std::{cmp::Ordering, str::FromStr};
+
 use axum::{
     extract::FromRequestParts,
     http::{HeaderMap, StatusCode, header::ToStrError, request::Parts},
     response::{IntoResponse, Response},
 };
-use mediatype::{MediaTypeError, MediaTypeList};
+use mediatype::{MediaType, MediaTypeError, MediaTypeList, Name, ReadParams, names::_STAR};
 
 #[doc(hidden)]
 pub use mediatype;
@@ -48,6 +54,8 @@ pub trait AssociatedMediaType {
 /// # Example
 ///
 /// ```rust
+/// use axum_accept::typed_media_type;
+///
 /// typed_media_type!(TextPlain: TEXT/PLAIN);
 /// ```
 #[macro_export]
@@ -55,7 +63,7 @@ macro_rules! typed_media_type {
     ($name:ident: $ty:ident/$subty:ident) => {
         pub struct $name($crate::mediatype::MediaType<'static>);
 
-        impl AssociatedMediaType for $name {
+        impl $crate::AssociatedMediaType for $name {
             fn new(media_type: $crate::mediatype::MediaType<'static>) -> Self {
                 if media_type != Self::associated_media_type() {
                     panic!("Attempted to create typed media type with non-matching inner value");
@@ -78,6 +86,8 @@ pub enum AcceptRejection {
     InvalidHeader(ToStrError),
     /// The media type at index .0 could not be parsed.
     InvalidMediaType(usize, MediaTypeError),
+    /// Invalid q parameter
+    InvalidQ(usize, <f64 as FromStr>::Err),
     /// No supported media type was found.
     NoSupportedMediaTypeFound,
 }
@@ -95,6 +105,10 @@ impl AcceptRejection {
                 StatusCode::BAD_REQUEST,
                 format!("Invalid media type in accept header at index {i}: {e}"),
             ),
+            Self::InvalidQ(i, e) => (
+                StatusCode::BAD_REQUEST,
+                format!("Invalid q parameter in accept header at index {i}: {e}"),
+            ),
             Self::NoSupportedMediaTypeFound => (
                 StatusCode::NOT_ACCEPTABLE,
                 "Accept header does not contain supported media types".to_string(),
@@ -109,14 +123,63 @@ impl IntoResponse for AcceptRejection {
     }
 }
 
-fn get_media_type_list(headers: &HeaderMap) -> Result<MediaTypeList, AcceptRejection> {
+fn get_media_type_list(headers: &HeaderMap) -> Result<Vec<MediaType<'_>>, AcceptRejection> {
     let accept_header = headers
         .get("accept")
         .map(|header| header.to_str())
         .transpose()
         .map_err(AcceptRejection::InvalidHeader)?
         .unwrap_or_default();
-    Ok(MediaTypeList::new(accept_header))
+
+    let q_name = Name::new("q").expect("Expected 'q' to be a valid name");
+    let mut list = MediaTypeList::new(accept_header)
+        .enumerate()
+        .map(|(i, mt)| match mt {
+            // validate q parameter and add it as u16 for sorting
+            Ok(mt) => Ok(match mt.get_param(q_name) {
+                Some(q_str) => {
+                    let q: f64 = q_str
+                        .as_str()
+                        .parse::<f64>()
+                        .map_err(|e| AcceptRejection::InvalidQ(i, e))?
+                        .clamp(0.0, 1.0);
+
+                    // q is clamped to 0.0-1.0 so nothing can happen here
+                    #[allow(clippy::cast_possible_truncation)]
+                    #[allow(clippy::cast_sign_loss)]
+                    ((q * 1000.0) as u16, mt)
+                }
+                None => (1000, mt),
+            }),
+            Err(e) => Err(AcceptRejection::InvalidMediaType(i, e)),
+        })
+        .collect::<Result<Vec<(u16, MediaType)>, AcceptRejection>>()?;
+
+    list.sort_by(|(a_q, a_mt), (b_q, b_mt)| {
+        if a_q == b_q {
+            // both have the same q, order by specificity
+
+            // is one of them */*? these come last
+            if (a_mt.ty, a_mt.subty) == (_STAR, _STAR) {
+                return Ordering::Greater;
+            } else if (b_mt.ty, b_mt.subty) == (_STAR, _STAR) {
+                return Ordering::Less;
+            }
+
+            // now check the subtype
+            if a_mt.subty != b_mt.subty {
+                if a_mt.subty == _STAR {
+                    return Ordering::Greater;
+                } else if b_mt.subty == _STAR {
+                    return Ordering::Less;
+                }
+            }
+        }
+
+        b_q.cmp(a_q)
+    });
+
+    Ok(list.into_iter().map(|(_, mt)| mt).collect())
 }
 
 /// Accept a single media type.
@@ -132,14 +195,7 @@ where
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
         let media_type_t = T::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_t {
                 return Ok(Accept(T::new(media_type_t)));
             }
@@ -175,14 +231,7 @@ where
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
         let media_type_a = A::associated_media_type();
         let media_type_b = B::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_a {
                 return Ok(Accept2::A(A::new(media_type_a)));
             } else if mt == media_type_b {
@@ -225,14 +274,7 @@ where
         let media_type_a = A::associated_media_type();
         let media_type_b = B::associated_media_type();
         let media_type_c = C::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_a {
                 return Ok(Accept3::A(A::new(media_type_a)));
             } else if mt == media_type_b {
@@ -282,14 +324,7 @@ where
         let media_type_b = B::associated_media_type();
         let media_type_c = C::associated_media_type();
         let media_type_d = D::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_a {
                 return Ok(Accept4::A(A::new(media_type_a)));
             } else if mt == media_type_b {
@@ -346,14 +381,7 @@ where
         let media_type_c = C::associated_media_type();
         let media_type_d = D::associated_media_type();
         let media_type_e = E::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_a {
                 return Ok(Accept5::A(A::new(media_type_a)));
             } else if mt == media_type_b {
@@ -417,14 +445,7 @@ where
         let media_type_d = D::associated_media_type();
         let media_type_e = E::associated_media_type();
         let media_type_f = F::associated_media_type();
-        for (i, mt) in get_media_type_list(&parts.headers)?.enumerate() {
-            let mt = match mt {
-                Ok(mt) => mt,
-                Err(e) => {
-                    return Err(AcceptRejection::InvalidMediaType(i, e));
-                }
-            };
-
+        for mt in get_media_type_list(&parts.headers)? {
             if mt == media_type_a {
                 return Ok(Accept6::A(A::new(media_type_a)));
             } else if mt == media_type_b {
@@ -447,4 +468,110 @@ where
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use mediatype::media_type;
+
+    #[test]
+    fn test_get_media_type_list_invisible_ascii() {
+        let mut headers = HeaderMap::new();
+        headers.insert("accept", "‎ ".parse().unwrap()); // invisible ascii is verboten
+        match get_media_type_list(&headers) {
+            Err(AcceptRejection::InvalidHeader(_)) => {}
+            _ => panic!("expected invalid header rejection"),
+        }
+    }
+
+    #[test]
+    fn test_get_media_type_list_invalid_media_type() {
+        let mut headers = HeaderMap::new();
+        headers.insert("accept", "lol".parse().unwrap());
+        match get_media_type_list(&headers) {
+            Err(AcceptRejection::InvalidMediaType(i, _)) => assert_eq!(i, 0),
+            _ => panic!("expected invalid media type rejection"),
+        }
+    }
+
+    #[test]
+    fn test_get_media_type_list_invalid_q() {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "accept",
+            "text/plain,application/json;q=lol".parse().unwrap(),
+        );
+        match get_media_type_list(&headers) {
+            Err(AcceptRejection::InvalidQ(i, _)) => assert_eq!(i, 1),
+            _ => panic!("expected invalid q rejection"),
+        }
+    }
+
+    #[test]
+    fn test_get_media_type_list_valid_types() {
+        let mut headers = HeaderMap::new();
+        headers.insert("accept", "text/plain".parse().unwrap());
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(vec![media_type!(TEXT / PLAIN)], list);
+
+        let mut headers = HeaderMap::new();
+        headers.insert("accept", "text/plain,application/json".parse().unwrap());
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(
+            vec![media_type!(TEXT / PLAIN), media_type!(APPLICATION / JSON)],
+            list
+        );
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "accept",
+            "text/plain,application/json;q=0.9".parse().unwrap(),
+        );
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(2, list.len());
+        assert_eq!(media_type!(TEXT / PLAIN), list[0]);
+        assert_eq!(media_type!(APPLICATION / JSON), list[1].essence());
+    }
+
+    #[test]
+    fn test_get_media_type_list_order() {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "accept",
+            "text/plain;q=0.9,application/json".parse().unwrap(),
+        );
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(2, list.len());
+        assert_eq!(media_type!(APPLICATION / JSON), list[0]);
+        assert_eq!(media_type!(TEXT / PLAIN), list[1].essence());
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "accept",
+            "text/*,text/plain,application/json".parse().unwrap(),
+        );
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(
+            vec![
+                media_type!(TEXT / PLAIN),
+                media_type!(APPLICATION / JSON),
+                media_type!(TEXT / _STAR)
+            ],
+            list
+        );
+
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "accept",
+            "*/*,text/*,text/plain,application/json".parse().unwrap(),
+        );
+        let list = get_media_type_list(&headers).expect("Accept header should've parsed correctly");
+        assert_eq!(
+            vec![
+                media_type!(TEXT / PLAIN),
+                media_type!(APPLICATION / JSON),
+                media_type!(TEXT / _STAR),
+                media_type!(_STAR / _STAR)
+            ],
+            list
+        );
+    }
+}
